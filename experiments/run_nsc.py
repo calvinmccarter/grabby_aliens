eebe24855a790904bd4126c5a98743de78b23965
calvinmccarter <mccarter.calvin@gmail.com>
2021-01-06 00:56:35 +0000

    fixing L=10

--- experiments/run_nsc.py
+++ experiments/run_nsc.py
@@ -5,19 +5,20 @@ import multiprocessing
 import subprocess
 
 D = 3
-N = 1e8
-nlist = [0.5, 1, 2, 5, 10, 20]
-c = 1.0
-slist = [0.01, 0.1, 0.2, 0.5, 0.8, 0.9, 0.99]
+N = 1e7
+nlist = [0.1, 0.2, 0.5, 1, 2, 3, 4, 5, 6, 10, 15, 20]
+s = 1.0
+sclist = [0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.99]
+L = 10.0
 
 n_perms = 1
 cmdlist = []
 for n in nlist:
-    for s in slist:
-        L = s
+    for sc in sclist:
+        c = s / sc
         for randix in range(n_perms):
-            fout = f"nsc/D={D}_n={float(n)}_N={N:.2e}_s={s:.5e}_L={float(L)}_c={float(c)}_r={randix}.csv"
-            cmd = f"../simulate {D} {n} {N} {s} {c} {L} > {fout}"
+            fout = f"nsc-s1L10/D={D}_n={float(n)}_N={N:.2e}_s={s:.1f}_L={L:.1f}_c={c:.5e}_r={randix}.csv"
+            cmd = f"../a.out {D} {n} {N} {s} {c} {L} > {fout}"
             print(cmd)
             cmdlist.append(cmd)
 max_processes = multiprocessing.cpu_count()
