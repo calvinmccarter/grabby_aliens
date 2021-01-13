f3e06e67fee5e95a1c4937151d6753be993c8c5e
calvinmccarter <calvin199@gmail.com>
2021-01-13 17:03:25 +0000

    simulate.cc assert

--- simulate.cc
+++ simulate.cc
@@ -172,6 +172,7 @@ vector<Civ> simulate(ll D, ld speed, ld n, ll N, ld c, ld L, ll empty_samples) {
   */
 
   SortedRNG R(N);
+  ld pi = 3.141592653589793238462643383279502884L;
   ll last_alive = 0;
   vector<Civ> ALIVE;
   for(ll i=0; i<N; i++) {
@@ -245,7 +246,8 @@ vector<Civ> simulate(ll D, ld speed, ld n, ll N, ld c, ld L, ll empty_samples) {
           ld angle_b = 1 + sq(speed/c);
           ld angle_a = (1.0 - sqrt(1.0 - angle_b*(1.0 - sq(dij/(c*dt)))))/angle_b;
           ld angle = 2*atan((speed/c)*(angle_a/(1-angle_a)));
-          assert(0.0 < angle && angle < 2.0);
+          assert(0.0 < angle);
+	  assert(angle < pi);
           c1.max_angle = max(c1.max_angle, angle);
         }
         c1.min_arrival = min(c1.min_arrival, arrival);
