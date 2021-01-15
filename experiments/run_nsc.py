1ef884176b05f1584afadf5d7089f9169198f494
calvinmccarter <calvin199@gmail.com>
2021-01-15 20:17:28 +0000

    figs

--- experiments/run_nsc.py
+++ experiments/run_nsc.py
@@ -5,22 +5,25 @@ import multiprocessing
 import subprocess
 
 D = 3
-N = 1e8
+N = 1e6
 true_nlist = [1.01, 1.1, 1.2, 1.5] + list(range(2, 21))
+print(true_nlist)
 m = 2/3
-nlist = [round((tnn-2*m)/(1-m)) for tnn in true_nlist]
+nlist = [round((tnn-2*m)/(1-m),4) for tnn in true_nlist]
+print(nlist)
 s = 1.0
 sclist = [0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.99]
+#sclist = [1.0]
 L = 1.0
 num_empty = 200
 
-n_perms = 5
+n_perms = 1
 cmdlist = []
 for n in nlist:
     for sc in sclist:
         c = s / sc
         for randix in range(n_perms):
-            fout = f"nsc-tuesday/D={D}_n={float(n)}_N={N:.2e}_s={s:.1f}_L={L:.1f}_c={c:.5e}_r={randix}"
+            fout = f"nsc-friday/D={D}_n={float(n)}_N={N:.2e}_s={s:.1f}_L={L:.1f}_c={c:.5e}_r={randix}"
             cmd = f"../a.out {D} {n} {N} {s} {c} {L} {fout} {randix} {num_empty}"
             print(cmd)
             cmdlist.append(cmd)
