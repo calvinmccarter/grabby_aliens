c47070ccced59ad3214b41774955899c614cfb54
Calvin McCarter <calvin199@gmail.com>
2021-02-07 15:40:59 -0600

    pre-merge

--- experiments/run_nsc.py
+++ experiments/run_nsc.py
@@ -5,16 +5,20 @@ import multiprocessing
 import subprocess
 
 D = 3
-N = 1e6
+N = 1e8
 true_nlist = [1.01, 1.1, 1.2, 1.5] + list(range(2, 21))
+true_nlist = [1.5, 2, 4, 8, 12, 16, 20, 24]
+true_nlist = [1.5, 3, 6]
 print(true_nlist)
-m = 2/3
-nlist = [round((tnn-2*m)/(1-m),4) for tnn in true_nlist]
+m = 0.90
+nlist = [round(tnn/(1-m)-1, 6) for tnn in true_nlist]
 print(nlist)
 s = 1.0
-sclist = [0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.99]
-#sclist = [1.0]
-L = 1.0
+#sclist = [0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.99, 1.0]
+#sclist = [0.01]
+#sclist = [0.2, 0.3, 0.4, 0.6, 0.7, 0.8, 0.9]
+sclist = [0.1, 0.5, 1.0]
+L = 1
 num_empty = 200
 
 n_perms = 1
@@ -23,11 +27,11 @@ for n in nlist:
     for sc in sclist:
         c = s / sc
         for randix in range(n_perms):
-            fout = f"nsc-friday/D={D}_n={float(n)}_N={N:.2e}_s={s:.1f}_L={L:.1f}_c={c:.5e}_r={randix}"
+            fout = f"nsc-setisaturday-numempty200/D={D}_n={float(n):.5f}_N={N:.2e}_s={s:.1f}_L={L:.1f}_c={c:.5e}_r={randix}"
             cmd = f"../a.out {D} {n} {N} {s} {c} {L} {fout} {randix} {num_empty}"
             print(cmd)
             cmdlist.append(cmd)
-max_processes = multiprocessing.cpu_count()
+max_processes = min(multiprocessing.cpu_count(), 4)
 processes = set()
 for cmd in cmdlist:
     #os.system(cmd)
